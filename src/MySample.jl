066a194532170452aed7df5476029de6a895cefe
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2019-05-05 11:20:30 +0900

    remove contents of access memory direction

--- src/MySample.jl
+++ src/MySample.jl
@@ -1,10 +1,8 @@
 module MySample
 
 export naiveform!, ipform!, opform!, mpform!
-export access_rows!, access_cols!
 export unroll_naive!, unroll_i!, unroll_j!, unroll_k!, unroll_ij!
 include("productform.jl")
-include("loopdirection.jl")
 include("unrolling.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -26,28 +26,6 @@ import Random
 end
 
 
-function preparedata(N::Int)
-    A = Array{Float64}(undef, N,N)
-    b = rand(N);
-    c = rand(N);
-    A, b, c
-end
-
-
-@testset "access array row/col direction" begin
-    Random.seed!(1234);
-    N = 10000
-    A, b, c = preparedata(N)
-
-    res_rows = @timed access_rows!(A, b, c)
-    res_cols = @timed access_cols!(A, b, c)
-    t1 = res_rows[2]
-    t2 = res_cols[2]
-    @show t1, t2
-    @test t1 <= t2
-end
-
-
 @testset "unrolling" begin
     N = 1000
     A = reshape(convert.(Float64, collect(1:N^2)), N, N)
